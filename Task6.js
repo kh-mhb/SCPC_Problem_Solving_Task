190a6916aac1bc4dd3d0f4a17584d239ea71a39e
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-07-25 13:57:15 +0600

    all task complete

---
+++ Task6.js
@@ -0,0 +1,23 @@
+function generateRandomPassword(length) {
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  
+    const allChars =
+      uppercaseChars + lowercaseChars + numberChars + specialChars;
+  
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allChars.length);
+      password += allChars.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+  
+  // Test the program
+  const passwordLength = 12;
+  const randomPassword = generateRandomPassword(passwordLength);
+  console.log(randomPassword);
+  
